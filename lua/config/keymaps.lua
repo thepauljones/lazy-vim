4eb551db6b7c5bf1d9936380ad08f478f9ad5382
Paul Jones <thepauljones@gmail.com>
2023-11-06 11:15:58 +0100

    Git blame <leader>gb, no relative lines

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
---
+
 -- Open fugitive on leader g
 vim.keymap.set("n", "<leader>g", "<Cmd>Git<CR>", { silent = true, noremap = true })
+
+-- leader gb shows blame
+vim.keymap.set("n", "<leader>gb", "<Cmd>Git blame<CR>", { silent = true, noremap = true })
 
 -- Stop ESC + j or k moving lines
 vim.keymap.del("n", "<A-j>")
